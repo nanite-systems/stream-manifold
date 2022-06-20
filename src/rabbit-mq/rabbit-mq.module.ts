f67e51262d798d35b686a7803122b67e5d535d18
Microwavekonijn <microwavekonijn@pm.me>
2022-06-20 21:43:12 +0200

    fix: rabbitmq stuck at boot

--- src/rabbit-mq/rabbit-mq.module.ts
+++ src/rabbit-mq/rabbit-mq.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Inject, Logger, Module, OnApplicationShutdown } from '@nestjs/common';
 import { ConfigModule } from '@census-reworked/nestjs-utils';
 import { RabbitMqConfig } from './rabbit-mq.config';
 import { INGRESS_QUEUE, RABBIT_MQ } from './constants';
 import { connect } from 'amqp-connection-manager';
 import { DataStreamFactory } from './factories/data-stream.factory';
+import { IAmqpConnectionManager } from 'amqp-connection-manager/dist/esm/AmqpConnectionManager';
 
 @Module({
   imports: [ConfigModule.forFeature([RabbitMqConfig])],
@@ -24,4 +25,25 @@ import { DataStreamFactory } from './factories/data-stream.factory';
   ],
   exports: [INGRESS_QUEUE],
 })
-export class RabbitMqModule {}
+export class RabbitMqModule implements OnApplicationShutdown {
+  private readonly logger = new Logger('RabbitMQ');
+
+  constructor(
+    @Inject(RABBIT_MQ) private readonly rabbitmq: IAmqpConnectionManager,
+  ) {
+    rabbitmq
+      .on('connect', () => {
+        this.logger.log('Connected');
+      })
+      .on('disconnect', () => {
+        this.logger.log('Disconnected');
+      })
+      .on('connectFailed', () => {
+        this.logger.log('Connection failed');
+      });
+  }
+
+  async onApplicationShutdown(): Promise<void> {
+    await this.rabbitmq.close();
+  }
+}
