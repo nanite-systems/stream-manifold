2a1fe856bdfa087c29a92c6dd25e2a6330d7fda8
Microwavekonijn <microwavekonijn@pm.me>
2022-06-25 23:00:15 +0200

    fix(stream): only catch validation errors

--- src/stream/interceptors/ignore-error.interceptor.ts
+++ src/stream/interceptors/ignore-error.interceptor.ts
@@ -1,5 +1,6 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
 import { catchError, from, Observable } from 'rxjs';
+import { ValidationError } from 'class-validator';
 
 export class IgnoreErrorInterceptor implements NestInterceptor {
   intercept(
@@ -7,8 +8,15 @@ export class IgnoreErrorInterceptor implements NestInterceptor {
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
     return next.handle().pipe(
-      catchError(() => {
-        return from([undefined]);
+      catchError((err) => {
+        if (
+          err instanceof Array &&
+          err.length &&
+          err[0] instanceof ValidationError
+        )
+          return from([undefined]);
+
+        throw err;
       }),
     );
   }
