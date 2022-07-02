dac8e58be901fa1d369103ad75954b9336652d71
Microwavekonijn <microwavekonijn@pm.me>
2022-07-03 00:28:44 +0200

    fix(environments): swapped ps4 eu and us world ids

--- src/environments/environment.manifest.ts
+++ src/environments/environment.manifest.ts
@@ -5,8 +5,8 @@ export class EnvironmentManifest {
 
   static readonly environments: Record<string, EnvironmentDescription> = {
     ps2: new EnvironmentDescription(['1', '10', '13', '17', '19', '40']),
-    ps2ps4eu: new EnvironmentDescription(['1000']),
-    ps2ps4us: new EnvironmentDescription(['2000']),
+    ps2ps4eu: new EnvironmentDescription(['2000']),
+    ps2ps4us: new EnvironmentDescription(['1000']),
   };
 
   static validateEnvironmentKey(key: string): boolean {
